02b25a579b7accf4eaeabc2cd91ced6a6f77a72c
ahmadgazalii <aghazali489@gmail.com>
2023-03-17 19:35:01 +0500

    routing app in nextjs13

---
+++ src/app/user/bob/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function MyComponent() {
+    return(
+
+    <div style={{color:'yellow'}}>hello bob</div>
+
+    )
+    // component logic here
+  }
+  
+  MyComponent.displayName = 'MyComponent';
+  
---
+++ src/app/user/jim/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function MyComponent() {
+    return(
+
+    <div style={{color:'blue'}}>hello jim</div>
+
+    )
+    // component logic here
+  }
+  
+  MyComponent.displayName = 'MyComponent';
+  
